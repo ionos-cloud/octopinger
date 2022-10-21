e3f5ad8e7394ce779dde05a60d4c8df1c61c797a
Sebastian Döll <sebastian@katallaxie.me>
2022-10-21 13:46:20 +0200

    chore: add post create script

---
+++ scripts/postCreateCommand.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script is executed after the creation of a new project.
+
+echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
+sudo apt update
+sudo apt install -y goreleaser
