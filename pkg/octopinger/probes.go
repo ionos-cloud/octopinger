58847f7c67b69033bda7427660670a2d454a4405
Sebastian Döll <sebastian@katallaxie.me>
2022-10-26 08:12:14 +0000

    wip: refactor prober

--- pkg/octopinger/probes.go
+++ pkg/octopinger/probes.go
@@ -5,26 +5,94 @@ import (
 	"time"
 
 	"github.com/go-ping/ping"
+	"golang.org/x/sync/errgroup"
 )
+
+type prober struct {
+	opts *Opts
+}
+
+// NewProber ...
+func NewProber(opt ...Opt) *prober {
+	options := new(Opts)
+	options.Configure(opt...)
+
+	p := new(prober)
+	p.opts = options
+
+	return p
+}
+
+// Prober ...
+type Prober interface {
+	Do(ctx context.Context, probe Probe) error
+}
+
+// Do ...
+func (p *prober) Do(ctx context.Context, probe Probe) func() error {
+	return func() error {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+			case <-ticker.C:
+				nodeList := NewNodeList()
+
+				err := nodeList.Load(p.opts.configPath, "nodes")
+				if err != nil {
+					return err
+				}
+
+				g, gctx := errgroup.WithContext(ctx)
+				g.SetLimit(10)
+
+				for _, n := range nodeList.Nodes() {
+					g.Go(func() error {
+						err := probe.Do(gctx, n)
+						if err != nil {
+							return err
+						}
+
+						return nil
+					})
+				}
+
+				err = g.Wait()
+				if err != nil {
+					return err
+				}
+
+				continue
+			}
+		}
+	}
+}
 
 // Probe ...
 type Probe interface {
 	// Do ...
-	Do(context.Context, string, time.Duration) error
+	Do(ctx context.Context, addr string) error
 }
 
 type icmpProbe struct {
+	opts *Opts
 }
 
 // NewICMPProbe ...
-func NewICMPProbe() *icmpProbe {
-	i := new(icmpProbe)
+func NewICMPProbe(opts ...Opt) *icmpProbe {
+	options := new(Opts)
+	options.Configure(opts...)
 
-	return i
+	p := new(icmpProbe)
+	p.opts = options
+
+	return p
 }
 
 // Do ...
-func (i *icmpProbe) Do(ctx context.Context, addr string, timeout time.Duration) error {
+func (i *icmpProbe) Do(ctx context.Context, addr string) error {
 	pinger, err := ping.NewPinger(addr)
 	if err != nil {
 		return err
--- pkg/octopinger/server.go
+++ pkg/octopinger/server.go
@@ -9,52 +9,79 @@ import (
 )
 
 type server struct {
-	configPath string
-	nodeName   string
-
-	monitor *Monitor
-	logger  *zap.Logger
+	opts *Opts
 	srv.Listener
 }
 
+// Opts ...
+type Opts struct {
+	monitor    *Monitor
+	logger     *zap.Logger
+	nodeName   string
+	configPath string
+	timeout    time.Duration
+	interval   time.Duration
+}
+
+// Configure ...
+func (o *Opts) Configure(opts ...Opt) {
+	for _, opt := range opts {
+		opt(o)
+	}
+}
+
 // Opt ...
-type Opt func(*server)
+type Opt func(*Opts)
+
+// WithMonitor ...
+func WithMonitor(m *Monitor) Opt {
+	return func(o *Opts) {
+		o.monitor = m
+	}
+}
 
 // WithLogger ...
 func WithLogger(logger *zap.Logger) Opt {
-	return func(s *server) {
-		s.logger = logger
+	return func(o *Opts) {
+		o.logger = logger
 	}
 }
 
 // WithConfigPath ...
 func WithConfigPath(path string) Opt {
-	return func(s *server) {
-		s.configPath = path
-	}
-}
-
-// WithMonitor ...
-func WithMonitor(m *Monitor) Opt {
-	return func(s *server) {
-		s.monitor = m
+	return func(o *Opts) {
+		o.configPath = path
 	}
 }
 
 // WithNodeName ...
 func WithNodeName(nodeName string) Opt {
-	return func(s *server) {
-		s.nodeName = nodeName
+	return func(o *Opts) {
+		o.nodeName = nodeName
+	}
+}
+
+// WithTimeout ...
+func WithTimeout(time time.Duration) Opt {
+	return func(o *Opts) {
+		o.timeout = time
+	}
+}
+
+// WithInterval ...
+func WithInterval(time time.Duration) Opt {
+	return func(o *Opts) {
+		o.interval = time
 	}
 }
 
 // NewServer ...
 func NewServer(opts ...Opt) *server {
-	s := new(server)
+	options := new(Opts)
+	options.Configure(opts...)
 
-	for _, opt := range opts {
-		opt(s)
-	}
+	s := new(server)
+	s.opts = options
 
 	return s
 }
@@ -62,40 +89,26 @@ func NewServer(opts ...Opt) *server {
 // Start ...
 func (s *server) Start(ctx context.Context, ready srv.ReadyFunc, run srv.RunFunc) func() error {
 	return func() error {
-		cfg, err := Config{}.Load(s.configPath)
+		cfg, err := Config{}.Load(s.opts.configPath)
 		if err != nil {
 			return err
 		}
 
-		nodeList := NewNodeList()
-
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
-
-		for {
-			select {
-			case <-ctx.Done():
-			case <-ticker.C:
-				err := nodeList.Load(s.configPath, "nodes")
-				if err != nil {
-					return err
-				}
-
-				healthy := true
-				for _, n := range nodeList.Nodes() {
-					if cfg.ICMP.Enabled {
-						icmp := NewICMPProbe()
-						err := icmp.Do(ctx, n, cfg.ICMP.Timeout)
-						if err != nil {
-							healthy = false
-						}
-					}
-				}
-
-				s.monitor.SetClusterHealthy(s.nodeName, healthy)
-
-				ticker.Reset(cfg.ICMP.Interval)
-			}
+		probes := []Probe{}
+		if cfg.ICMP.Enabled {
+			probes = append(probes, NewICMPProbe(WithMonitor(s.opts.monitor)))
 		}
+
+		for _, probe := range probes {
+			p := NewProber(
+				WithMonitor(s.opts.monitor),
+				WithConfigPath(s.opts.configPath),
+			)
+			run(p.Do(ctx, probe))
+		}
+
+		<-ctx.Done()
+
+		return nil
 	}
 }
