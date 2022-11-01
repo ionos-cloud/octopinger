4c691c1ad9eca95dad7354ec085a0d645bfe6b19
Sebastian Döll <sebastian@katallaxie.me>
2022-11-01 10:28:38 +0000

    chore: set env variable

--- internal/tools/doc.go
+++ internal/tools/doc.go
@@ -6,6 +6,7 @@ package tools
 import (
 	_ "github.com/golang/mock/mockgen/model"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/helm/chart-releaser/cr"
 	_ "mvdan.cc/gofumpt"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v3"
