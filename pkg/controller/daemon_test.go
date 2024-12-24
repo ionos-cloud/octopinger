147b3d1099735a458bdaf3230b74a243dbfa4ef2
Paul Eipel <eipel@strato.de>
2024-12-24 11:08:02 +0100

    new release version v0.1.12

--- pkg/controller/daemon_test.go
+++ pkg/controller/daemon_test.go
@@ -41,7 +41,7 @@ var _ = Describe("DaemonSet controller", func() {
 				},
 				Spec: v1alpha1.OctopingerSpec{
 					Template: v1alpha1.Template{
-						Image: "ghcr.io/ionos-cloud/octopinger/octopinger:v0.1.11",
+						Image: "ghcr.io/ionos-cloud/octopinger/octopinger:v0.1.12",
 					},
 					Config: v1alpha1.Config{
 						ICMP: v1alpha1.ICMP{
